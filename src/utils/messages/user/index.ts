87b1d45c6752a059f3a484dc1c47fe66ab741554
Aoneahsan <aoneahsan@gmail.com>
2024-08-16 18:49:04 +0500

    updated and pushed

--- src/utils/messages/user/index.ts
+++ src/utils/messages/user/index.ts
@@ -2,6 +2,7 @@ const user = {
     deletedSuccessfully: 'User deleted successfully.',
     blockSuccessfully: 'User block successfully.',
     updatedSuccessfully: 'User updated successfully.',
+    createdSuccessfully: 'User created successfully.',
     unBlockSuccessfully: 'User unblock successfully.',
     notFound: 'User not found.',
     noIdProvided: 'No user id provided!'

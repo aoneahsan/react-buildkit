2454a97b3bdbdedb880e0856a1688266c89c7813
Ahsan Mahmood <aoneahsan@gmail.com>
2024-10-13 18:03:19 +0500

    updated version deployed

--- src/packages/ReactTable.ts
+++ src/packages/ReactTable.ts
@@ -1,58 +1,60 @@
 export enum TableIdsEnum {
-    // Generic
-    id = 'tableIdCol',
-    title = 'tableTitleCol',
-    description = 'tableDescriptionCol',
-    image = 'tableImageCol',
-    status = 'tableStatusCol',
-    amount = 'tableAmountCol',
-    actions = 'tableActionsCol',
-    createdAt = 'tableCreatedAtCol',
-    transferMethod = 'tableTransferMethodCol',
-    receptScreenshot = 'tableReceptScreenshotCol',
+  // Generic
+  id = 'tableIdCol',
+  title = 'tableTitleCol',
+  description = 'tableDescriptionCol',
+  image = 'tableImageCol',
+  status = 'tableStatusCol',
+  amount = 'tableAmountCol',
+  actions = 'tableActionsCol',
+  createdAt = 'tableCreatedAtCol',
+  transferMethod = 'tableTransferMethodCol',
+  receptScreenshot = 'tableReceptScreenshotCol',
 
-    // User
-    name = 'tableNameCol',
-    email = 'tableEmailCol',
-    phoneNumber = 'tablePhoneNumberCol',
-    city = 'tableCityCol',
-    role = 'tableRoleCol',
-    country = 'tableCountryCol',
-    referralCode = 'tableReferralCodeCol',
-    withdrawOptions = 'tableWithdrawOptionsCol',
-    isBlocked = 'tableIsBlockedCol',
+  // User
+  name = 'tableNameCol',
+  email = 'tableEmailCol',
+  phoneNumber = 'tablePhoneNumberCol',
+  city = 'tableCityCol',
+  role = 'tableRoleCol',
+  country = 'tableCountryCol',
+  referralCode = 'tableReferralCodeCol',
+  withdrawOptions = 'tableWithdrawOptionsCol',
+  isBlocked = 'tableIsBlockedCol',
 
-    // Games & Game room
-    maxPlayersAllowed = 'tableMaxPlayersAllowedCol',
-    minPlayersToStartGame = 'tableMinPlayersToStartGameCol',
-    feePerPlayer = 'tableFeePerPlayerCol',
-    serviceCharges = 'tableServiceChargesCol',
-    engagerServiceCharges = 'tableEngagerServiceChargesCol',
-    roomCode = 'tableRoomCodeCol',
-    gameTitle = 'tableGameTitleCol',
-    private = 'tablePrivateCol',
-    visibility = 'tableVisibilityCol',
-    playersCount = 'tablePlayersCountCol',
-    willStartAt = 'tableWillStartAtCol',
+  // Games & Game room
+  maxPlayersAllowed = 'tableMaxPlayersAllowedCol',
+  minPlayersToStartGame = 'tableMinPlayersToStartGameCol',
+  feePerPlayer = 'tableFeePerPlayerCol',
+  serviceCharges = 'tableServiceChargesCol',
+  engagerServiceCharges = 'tableEngagerServiceChargesCol',
+  roomCode = 'tableRoomCodeCol',
+  gameTitle = 'tableGameTitleCol',
+  private = 'tablePrivateCol',
+  visibility = 'tableVisibilityCol',
+  playersCount = 'tablePlayersCountCol',
+  willStartAt = 'tableWillStartAtCol',
+  capacity = 'capacity',
+  poolValue = 'poolValue',
 
-    // Notification
-    sendTo = 'tableSendToCol',
-    sendAt = 'tableSendAtCol',
+  // Notification
+  sendTo = 'tableSendToCol',
+  sendAt = 'tableSendAtCol',
 
-    // Top up
-    agentRemarks = 'tableAgentRemarksCol',
-    playerData = 'tablePlayerDataCol',
-    requestAt = 'tableRequestAtCol',
+  // Top up
+  agentRemarks = 'tableAgentRemarksCol',
+  playerData = 'tablePlayerDataCol',
+  requestAt = 'tableRequestAtCol',
 
-    // Transfer coin
-    playerPhoneNumber = 'tablePlayerPhoneNumberCol',
-    transferAt = 'tableTransferAtCol',
+  // Transfer coin
+  playerPhoneNumber = 'tablePlayerPhoneNumberCol',
+  transferAt = 'tableTransferAtCol',
 
-    // Withdraw request
-    requestedAt = 'tableRequestedAtCol',
-    playerEmail = 'tablePlayerEmailCol',
+  // Withdraw request
+  requestedAt = 'tableRequestedAtCol',
+  playerEmail = 'tablePlayerEmailCol',
 
-    //
-    roleIdentifier = 'roleIdentifier',
-    isDefault = 'isDefault',
+  //
+  roleIdentifier = 'roleIdentifier',
+  isDefault = 'isDefault',
 }
